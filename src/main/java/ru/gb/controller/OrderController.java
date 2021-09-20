08e7180e209726db04234916a03d152452c89964
noriksaroyan <noriksaroyan@gmail.com>
2021-09-20 21:33:59 +0300

    first commit

---
+++ src/main/java/ru/gb/controller/OrderController.java
@@ -0,0 +1,41 @@
+package ru.gb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.gb.entity.Order;
+import ru.gb.repository.OrderRepository;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderRepository repository;
+
+
+    public OrderController(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Order findById(@PathVariable int id) {
+        return repository.findById(id).isPresent() ? repository.findById(id).get() : null;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public List<Order> save(@RequestBody Order order) {
+        repository.add(order);
+        return repository.findAll();
+    }
+}
---
+++ src/test/java/ru/gb/HW4/Hw4ApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.gb.HW4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw4ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
